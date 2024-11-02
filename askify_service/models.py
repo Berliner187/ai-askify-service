5be1ebae4cff3fddcf826ce605cf9f4285b55c23
Rostislav <fadenougat@gmail.com>
2024-11-02 22:14:37 +0300

    0.4.1: Patch
    - Вынесен метод преобразования тестов в формат PDF с возможностью скачивания
    - Добавлены дополнительные методы с интеграцией других LLM моделей

--- askify_service/models.py
+++ askify_service/models.py
@@ -5,12 +5,7 @@ import json
 import uuid
 
 from django.http import HttpResponse
-from django.contrib.staticfiles import finders
-from reportlab.lib import colors
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.pdfgen import canvas
+
 
 from .utils import *
 
@@ -31,67 +26,14 @@ class Survey(models.Model):
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = f'attachment; filename="тест.pdf"'
 
-        p = canvas.Canvas(response, pagesize=letter)
+        converter_pdf = ConverterPDF()
+        response = converter_pdf.get_survey_in_pdf(response, self.title, self.get_questions())
 
-        font_path_medium = finders.find('fonts/Manrope-Medium.ttf')
-        font_path_bold = finders.find('fonts/Manrope-ExtraBold.ttf')
-        font_signature = finders.find('fonts/Unbounded-Medium.ttf')
-
-        pdfmetrics.registerFont(TTFont('Manrope Medium', font_path_medium))
-        pdfmetrics.registerFont(TTFont('Manrope Bold', font_path_bold))
-        pdfmetrics.registerFont(TTFont('Unbounded Medium', font_signature))
-
-        p.setFont('Manrope Bold', 16)
-        p.drawString(80, 750, f"{self.title}")
-
-        p.setFont('Manrope Medium', 11)
-
-        questions = self.get_questions()
-        y = 700
-
-        for question in questions:
-            question_text = question['question']
-            options = question['options']
-
-            text_object = p.beginText(80, y)
-            text_object.setFont('Manrope Medium', 11)
-            text_object.setTextOrigin(80, y)
-
-            max_width = 400
-
-            for line in question_text.splitlines():
-                words = line.split(' ')
-                current_line = ''
-                for word in words:
-                    if p.stringWidth(current_line + word + ' ', 'Manrope Medium', 11) < max_width:
-                        current_line += word + ' '
-                    else:
-                        text_object.textLine(current_line)
-                        current_line = word + ' '
-                        y -= 15
-                text_object.textLine(current_line)
-
-            p.drawText(text_object)
-            y -= 20
-
-            count = 1
-            for option in options:
-                p.drawString(100, y, f"{count}) {option}")
-                y -= 15
-                count += 1
-
-            y -= 10
-
-        p.setFont('Unbounded Medium', 7)
-        p.drawString(80, 40, f"Created by Летучка, {get_year_now()}")
-
-        p.showPage()
-        p.save()
         return response
 
 
 class UserAnswers(models.Model):
-    survey_id = models.UUIDField()  # Поле для общего survey_id
+    survey_id = models.UUIDField()
     selected_answer = models.CharField(max_length=255)
     scored_points = models.IntegerField()
     total_points = models.IntegerField()
--- askify_service/utils.py
+++ askify_service/utils.py
@@ -1,7 +1,16 @@
 import json
-import openai
-
 import datetime
+
+import openai
+import requests
+
+from django.contrib.staticfiles import finders
+
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
 
 
 class ManageConfidentFields:
@@ -29,24 +38,28 @@ class GenerationModelsControl:
     def __init__(self):
         pass
 
-    def get_service_0001(self, text_from_user):
+    @staticmethod
+    def __get_confidential_key(key_name):
         manage_confident_fields = ManageConfidentFields("config.json")
+        return manage_confident_fields.get_confident_key(key_name)
+
+    def get_service_0001(self, text_from_user):
 
         client = openai.OpenAI(
-            api_key=manage_confident_fields.get_confident_key("api_openai"),
+            api_key=self.__get_confidential_key("api_openai"),
             base_url="https://glhf.chat/api/openai/v1",
         )
 
         completion = client.chat.completions.create(
-            model=f"hf:{manage_confident_fields.get_confident_key('llm_model_name')}",
+            model=f"hf:{self.__get_confidential_key('llm_model_name')}",
             messages=[
                 {
                     "role": "system",
-                    "content": f"{manage_confident_fields.get_confident_key('system_prompt')}"
+                    "content": f"{self.__get_confidential_key('system_prompt')}"
                 },
                 {
                     "role": "user",
-                    "content": f"{text_from_user}{manage_confident_fields.get_confident_key('user_prompt')}"
+                    "content": f"{text_from_user}{self.__get_confidential_key('user_prompt')}"
                 }
             ]
         )
@@ -55,6 +68,101 @@ class GenerationModelsControl:
         cleaned_generated_text = generated_text.replace("json", "").replace("`", "")
 
         return cleaned_generated_text
+
+    def response_service_0002(self, text_from_user):
+        messages = [
+            {
+                "role": "system",
+                "content": f"{self.__get_confidential_key('system_prompt')}"
+            },
+            {
+                "role": "user",
+                "content": f"{text_from_user}{self.__get_confidential_key('user_prompt')}"
+            }
+        ]
+
+        url = "https://api.arliai.com/v1/chat/completions"
+        payload = json.dumps({
+            "model": "Meta-Llama-3.1-8B-Instruct",
+            "messages": messages,
+            "temperature": 0.7,
+            "max_tokens": 1024,
+            "stream": False
+        })
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.__get_confidential_key("api_arliai")}'
+        }
+        response = requests.post(url, headers=headers, data=payload)
+        return response.json()
+
+    def get_service_0002(self, text_from_user):
+        completion = self.response_service_0002(text_from_user)
+
+        assistant_response = completion['choices'][0]['message']['content']
+        print("Assistant's response:", assistant_response)
+        return assistant_response
+
+
+class ConverterPDF:
+    @staticmethod
+    def get_survey_in_pdf(response, title, questions):
+        p = canvas.Canvas(response, pagesize=letter)
+
+        font_path_medium = finders.find('fonts/Manrope-Medium.ttf')
+        font_path_bold = finders.find('fonts/Manrope-ExtraBold.ttf')
+        font_signature = finders.find('fonts/Unbounded-Medium.ttf')
+
+        pdfmetrics.registerFont(TTFont('Manrope Medium', font_path_medium))
+        pdfmetrics.registerFont(TTFont('Manrope Bold', font_path_bold))
+        pdfmetrics.registerFont(TTFont('Unbounded Medium', font_signature))
+
+        p.setFont('Manrope Bold', 16)
+        p.drawString(80, 750, f"{title}")
+
+        p.setFont('Manrope Medium', 11)
+
+        y = 700
+
+        for question in questions:
+            question_text = question['question']
+            options = question['options']
+
+            text_object = p.beginText(80, y)
+            text_object.setFont('Manrope Medium', 11)
+            text_object.setTextOrigin(80, y)
+
+            max_width = 400
+
+            for line in question_text.splitlines():
+                words = line.split(' ')
+                current_line = ''
+                for word in words:
+                    if p.stringWidth(current_line + word + ' ', 'Manrope Medium', 11) < max_width:
+                        current_line += word + ' '
+                    else:
+                        text_object.textLine(current_line)
+                        current_line = word + ' '
+                        y -= 15
+                text_object.textLine(current_line)
+
+            p.drawText(text_object)
+            y -= 20
+
+            count = 1
+            for option in options:
+                p.drawString(100, y, f"{count}) {option}")
+                y -= 15
+                count += 1
+
+            y -= 10
+
+        p.setFont('Unbounded Medium', 7)
+        p.drawString(80, 40, f"Created by Летучка, {get_year_now()}")
+
+        p.showPage()
+        p.save()
+        return response
 
 
 def get_year_now():
